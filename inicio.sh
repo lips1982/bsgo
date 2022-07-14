addee597d971a20a0bd196af68865c35de47bd25
lips1982 <42499047+lips1982@users.noreply.github.com>
2022-07-13 21:41:07 -0500

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y install screen
 
 docker build -t display .
-
+mkdir -p img
 screen -S docker -d -m bash -c "docker run -it --rm -v $PWD/img:/app/Almacenamiento/img display"
 screen -S web -d -m bash -c "cd img/ && python3 -m http.server 8080"
 
